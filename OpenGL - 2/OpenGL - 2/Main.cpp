86c8a345cedd4549b254fba311cdcf4f1e52540a
YNTCG <yevgeni22@gmail.com>
2025-11-09 14:08:58 +0200

    new

--- OpenGL - 2/OpenGL - 2/Main.cpp
+++ OpenGL - 2/OpenGL - 2/Main.cpp
@@ -33,7 +33,7 @@ int main()
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	// Create a GLFWwindow object of 800 by 800 pixels, naming it "YoutubeOpenGL"
-	GLFWwindow* window = glfwCreateWindow(800, 800, "YoutubeOpenGL", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(800, 800, "OpenGL - 2", NULL, NULL);
 	// Error check if the window fails to create
 	if (window == NULL)
 	{
