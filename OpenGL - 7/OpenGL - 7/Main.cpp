e4ae08260baecdbecb3990f5f81c37073bf3b2ea
YNTCG <yevgeni22@gmail.com>
2025-11-11 21:48:04 +0200

    New items

--- OpenGL - 7/OpenGL - 7/Main.cpp
+++ OpenGL - 7/OpenGL - 7/Main.cpp
@@ -111,7 +111,7 @@ int main()
 	* Also note that this requires C++17, so go to Project Properties, C/C++, Language, and select C++17
 	*/
 	std::string parentDir = (fs::current_path().fs::path::parent_path()).string();
-	std::string texPath = "/Resources/OpenGL- 7 - Going 3D/";
+	std::string texPath = "/Resources/OpenGL - 7 - Going 3D/";
 
 	// Texture
 	Texture brickTex((parentDir + texPath + "brick.png").c_str(), GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
